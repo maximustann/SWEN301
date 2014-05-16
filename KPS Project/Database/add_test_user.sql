b0053bdbf6af1b69a91dd1116e014e36232cdbe6
scottbolten <sdbolten@gmail.com>
2014-05-17 08:50:44 +1200

    comit

---
+++ KPS Project/Database/add_test_user.sql
@@ -0,0 +1,6 @@
+INSERT INTO UserDetails(Username, Password, IsAdmin) VALUES ("Peter", "234567", 1);
+INSERT INTO UserDetails(Username, Password, IsAdmin) VALUES ("Andrew", "987654", 0);
+INSERT INTO UserDetails(Username, Password, IsAdmin) VALUES ("Griffin", "808099", 0);
+INSERT INTO UserDetails(Username, Password, IsAdmin) VALUES ("Luis", "102938", 0);
+INSERT INTO UserDetails(Username, Password, IsAdmin) VALUES ("David", "987654", 0);
+INSERT INTO UserDetails(Username, Password, IsAdmin) VALUES ("Chris", "098765", 0);
