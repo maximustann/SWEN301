c7ae3e86b2e223c5dc711f02c7f1e5fc5612ab98
Max <titantbx1989215@gmail.com>
2014-05-17 17:17:27 +1200

    update database

--- Database/create.sql
+++ Database/create.sql
@@ -1,25 +1,60 @@
 CREATE TABLE BusinessEvents(
 	ID INTEGER PRIMARY KEY NOT NULL,
 	EventTypeID INT,
-	Origin INT,
-	Destination INT,
+	Origin INT REFERENCES Cities(ID),
+	Destination INT REFERENCES Cities(ID),
 	Weight REAL,
 	Volume REAL,
-	TimeOfEntry DATETIME,
+	TimeOfEntry INTEGER,
 	Priority INT,
 	PricePerGram REAL,
 	PricePerCC REAL,
-	Firm Text,
+	Company INT REFERENCES Company(ID),
 	TransportType TEXT,
 	DayOfWeek INT,
 	Frequency REAL,
 	Duration REAL);
 CREATE TABLE EventTypes(
-	Event TEXT NOT NULL,
-	EventTypeID INTEGER NOT NULL);
+	EventTypeID INTEGER PRIMARY KEY,
+	Event TEXT NOT NULL);
 CREATE TABLE TransportRoutes(
-	ID INTEGER PRIMARY KEY NOT NULL,
-	EventID INT );
+	ID INTEGER 	PRIMARY KEY,
+	Origin INT REFERENCES Cities(ID),
+	Destination INT REFERENCES Cities(ID),
+	Company INT REFERENCES company(ID),
+	TransportType TEXT,
+	DeliverDay INT,
+	PricePerGram REAL,
+	PricePerCC REAL,
+	Frequency REAL
+	Duration REAL);
 CREATE TABLE CustomerRoutes(
-	ID INTEGER PRIMARY KEY NOT NULL,
-	EventID INT);
+	ID INTEGER PRIMARY KEY,
+	Origin INT REFERENCES Cities(ID),
+	Destination INT REFERENCES Cities(ID),
+	Priority INT,
+	TransportType TEXT,
+	PricePerGram REAL,
+	PricePerCC REAL,
+);
+
+CREATE TABLE Mail(
+	ID INTEGER PRIMARY KEY,
+	Origin INT REFERENCES Cities(ID),
+	Destination INT REFERENCES Cities(ID),
+	costKPS REAL,
+	costClient REAL,	
+	Priority INT,
+	Volume REAL,
+	Weight	REAL,
+	TimeOfEntry INTEGER,
+);
+
+CREATE TABLE Cities(
+	ID INTEGER PRIMARY KEY,
+	Name TEXT NOT NULL
+);
+CREATE TABLE Company(
+	ID INTEGER PRIMARY KEY,
+	Name TEXT NOT NULL
+);
