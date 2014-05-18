4853f271ec81470de1b8407c1a792054b56d8a25
NickMcNeil <nickmcneil93@gmail.com>
2014-05-18 20:04:10 +1200

    Last of these hellish commits

--- KPS Project/Validation.py
+++ KPS Project/Validation.py
@@ -26,7 +26,7 @@ def validate(eventDict):
             errorMessages.append("Weight not in real format");
             
         # check greater than zero
-        if not(eventDict.get("Weight") > 0):
+        if not(float(eventDict.get("Weight")) > 0):
             errorMessages.append("Weight must be greater than zero");
 
     if(eventDict.get("Volume") != None):
@@ -35,7 +35,7 @@ def validate(eventDict):
             errorMessages.append("Volume not in real format");
             
         # check greater than zero
-        if not(eventDict.get("Volume") > 0):
+        if not(float(eventDict.get("Volume")) > 0):
             errorMessages.append("Volume must be greater than zero");
            
     if(eventDict.get("TimeOfEntry") != None):
@@ -48,17 +48,15 @@ def validate(eventDict):
         if not (isinstance(eventDict.get("Priority"), int)):
             errorMessages.append("Priority not in int format");   
         # check priority either 1 or 2
-        elif(eventDict.get("Priority") != 1 and eventDict.get("Priority") != 2):
+        elif(float(eventDict.get("Priority")) != 1 and float(eventDict.get("Priority")) != 2):
             errorMessages.append("Priority must be either 1 or 2");
            
     if(eventDict.get("PricePerGram") != None):
         # check correct format
-        
-        
         if not (valueToFloat(eventDict.get("PricePerGram"))):
             errorMessages.append("PricePerGram not in real format");
         # check greater than zero
-        elif not(eventDict.get("PricePerGram") > 0):
+        elif not(float(eventDict.get("PricePerGram")) > 0):
             errorMessages.append("PricePerGram must be greater than zero");
            
     if(eventDict.get("PricePerCC") != None):
@@ -66,7 +64,7 @@ def validate(eventDict):
         if not (valueToFloat(eventDict.get("PricePerCC"))):
             errorMessages.append("PricePerCC not in real format");
         # check greater than zero
-        elif not(eventDict.get("PricePerCC") > 0):
+        elif not(float(eventDict.get("PricePerCC")) > 0):
             errorMessages.append("PricePerCC must be greater than zero");
            
     if(eventDict.get("Firm") != None):
@@ -87,14 +85,14 @@ def validate(eventDict):
         if not (valueToFloat(eventDict.get("DayOfWeek"))):
             errorMessages.append("DayOfWeek not in int format");
         # day of the week must be between 1 and 7
-        elif (eventDict.get("DayOfWeek")>7 or eventDict.get("DayOfWeek")<1):
+        elif (float(eventDict.get("DayOfWeek"))>7 or float(eventDict.get("DayOfWeek"))<1):
             errorMessages.append("DayOfWeek must be between 1 and 7. Actual output: " + str(eventDict.get("DayOfWeek")))
              
     if(eventDict.get("Frequency") != None):
         # check correct format
         if not (valueToFloat(eventDict.get("Frequency"))):
             errorMessages.append("Frequency not in real format");
-        elif not (eventDict.get("Frequency")>0):
+        elif not (float(eventDict.get("Frequency")>0)):
             errorMessages.append("Frequency must be greater than 0");
            
     if(eventDict.get("Duration") != None):
@@ -102,7 +100,7 @@ def validate(eventDict):
         if not (valueToFloat(eventDict.get("Duration"))):
             errorMessages.append("Duration not in real format");    
         # check greater than zero
-        elif not(eventDict.get("Duration") > 0):
+        elif not(float(eventDict.get("Duration")) > 0):
             errorMessages.append("Duration must be greater than zero");
            
     return errorMessages
@@ -110,7 +108,6 @@ def validate(eventDict):
 def valueToFloat(value):
     try:
         float(value)
-        value = float(value)
         return True;
     except ValueError:
         return False
