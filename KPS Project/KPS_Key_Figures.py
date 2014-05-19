bf92e0a144354873fde4908edfb3234a4b157e22
wipethefridge <wipethefridge@gmail.com>
2014-05-19 15:35:03 +1200

    Key figures again...
    Just making the table a bit more informative

--- KPS Project/KPS_Key_Figures.py
+++ KPS Project/KPS_Key_Figures.py
@@ -9,7 +9,7 @@ import KeyEventsCommandHandler as events_handler
 from ui import Ui_KPS_KeyFigures as ui_key_figures;
 
 class Database:
-    def __init__(self, parent = None):
+    def __init__(self, parent = Qt.QObject):
         self.data = QSqlDatabase.addDatabase("QSQLITE")
         self.data.setDatabaseName("../Database/Business.db")
         self.data.open()
@@ -30,15 +30,13 @@ class Key_Figures_Dialog(QtGui.QDialog):
         self.ui.tableView_2.setModel(self.criticalRoutes())
         self.ui.tableView_3.setModel(self.averageDelTime())
         
-        #self.ui.tableView.show()
-        
     def totalRevenue(self):
         tup = events_handler.getTotalRevenue()
-        return tup[0]
+        return str(tup[0])
     
     def totalExpenditure(self):
         tup = events_handler.getTotalExpenditure()
-        return tup[0]
+        return str(tup[0])
     
     def totalNoEvents(self):
         tup = events_handler.getTotalNumberOfEvents()
@@ -46,9 +44,15 @@ class Key_Figures_Dialog(QtGui.QDialog):
     
     def amountOfMail(self):
         model = QSqlQueryModel()
-        model.setQuery('''SELECT origin, destination, sum(volume), sum(weight), count(*)
-                    FROM mail
-                    group by origin''')
+        model.setQuery('''SELECT C.Name, R.Name, R.Volume, R.Weight, R.Count
+                FROM(SELECT Q.Origin, C.Name, Q.Volume, Q.Weight, Q.Count
+                    FROM(SELECT Origin, Destination, sum(Volume) as Volume, sum(Weight) as Weight, Count(*) as Count
+                        FROM Mail
+                        GROUP BY Origin, Destination) as Q, Cities as C
+                    WHERE C.ID = Q.Destination) as R, Cities as C
+                WHERE C.ID = R.Origin;''')
+        model.setHeaderData(0, Qt.Qt.Horizontal, "Origin")
+        model.setHeaderData(1, Qt.Qt.Horizontal, "Destination")
         model.setHeaderData(2, Qt.Qt.Horizontal, "Total Volume")
         model.setHeaderData(3, Qt.Qt.Horizontal, "Total Weight")
         model.setHeaderData(4, Qt.Qt.Horizontal, "Total Count")
@@ -70,12 +74,19 @@ class Key_Figures_Dialog(QtGui.QDialog):
         model.setHeaderData(3, Qt.Qt.Horizontal, "Average Loss")
         return model
     
-    def averageDelTime(self): # this doesn't work if the DeliverTime column doesn't exist...
+    def averageDelTime(self):
         model = QSqlQueryModel()
-        model.setQuery('''SELECT origin, destination, priority, avg(DeliverTime)
-                FROM mail
-                group by origin, destination, priority;''')
-        model.setHeaderData(3, Qt.Qt.Horizontal, "Time")
+        model.setQuery('''SELECT C.Name, R.Name, R.Priority, R.Time
+                FROM(SELECT Q.Origin, C.Name, P.Priority, Q.Time
+                    FROM(SELECT Origin, Destination, Priority, avg(DeliverTime) as Time
+                        FROM Mail
+                        GROUP BY Origin, Destination, Priority) as Q, Priorities as P, Cities as C
+                    WHERE P.ID = Q.Priority AND C.ID = Q.Destination) as R, Cities as C
+                WHERE C.ID = R.Origin;''')
+        model.setHeaderData(0, Qt.Qt.Horizontal, "Origin")
+        model.setHeaderData(1, Qt.Qt.Horizontal, "Destination")
+        model.setHeaderData(2, Qt.Qt.Horizontal, "Priority")
+        model.setHeaderData(3, Qt.Qt.Horizontal, "Average Time")
         return model
 
 if __name__ == "__main__":
